37e8f5bdd31400a1070cbf55723e5c746e81e43f
nicoasensio <124016803+nicoasensio@users.noreply.github.com>
2023-01-31 00:29:23 -0300

    Add files via upload

---
+++ Funcion.js
@@ -0,0 +1,11 @@
+
+   function funcion1() {
+    document.getElementById("universidad").innerHTML = "Universitarios: Licenciatura en ciencias de la computación - Universidad Nacional de San Luis";
+  }
+  function funcion2() {
+    document.getElementById("primaria").innerHTML = "Primarios: Colegio N°7 - Completados";
+    
+  }
+   function funcion3() {
+    document.getElementById("secundaria").innerHTML = "Secundarios: Colegio N°7 - Completados";
+   }
